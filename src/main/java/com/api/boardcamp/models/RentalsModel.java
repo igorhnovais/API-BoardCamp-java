5d082d1e4300bc22487ccd763245dae6d955f726
Igor Novais <igorhnovaisdriven@gmail.com>
2024-02-05 11:00:18 -0300

    feat: add models

---
+++ src/main/java/com/api/boardcamp/models/RentalsModel.java
@@ -0,0 +1,54 @@
+package com.api.boardcamp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rentals")
+
+public class RentalsModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private Number customerId;
+
+    @Column(nullable = false)
+    private Number GameId;
+
+    @Column(nullable = false)
+    private String rentDate;
+
+    @Column(nullable = false)
+    private Number daysRented;
+
+    @Column(nullable = false)
+    private Boolean returnDate;
+
+    @Column(nullable = false)
+    private Number originalPrice;
+
+    @Column(nullable = false)
+    private Number delayFree;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId")
+    private CustomerModel customer;
+
+    @ManyToOne
+    @JoinColumn(name = "gameId")
+    private GameModel game;
+}
